d5a49b1968a02633e626e7f03b805f955e1b7d21
marceterrone10 <marcelo.terrone10@gmail.com>
2025-07-22 11:33:49 -0300

    Login auth.controller already done

--- src/api/controllers/auth.controller.js
+++ src/api/controllers/auth.controller.js
@@ -36,3 +36,43 @@ export const register = async (req, res) => {
         res.status(500).json({ message: 'Error registering user' });
     }
 };
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await Usuario.find({ email });
+
+        if (!user) {
+            return res.status(400).json({
+                message: 'User not found'
+            });
+        };
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({
+                message: 'Invalid credentials'
+            });
+        };
+
+        // Now, generate a JWT token
+        const token = jwt.sign(
+            {   id: user._id,
+                email: user.email,
+                rol: user.rol
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' } // Token will expire in 1 hour
+        );
+        res.status(200).json({
+            message: 'Login successful',
+            token
+        });
+        console.log("User logged in successfully");
+
+    } catch (error) {
+        console.log("Failed to login user:", error);
+        res.status(500).json({ message: 'Error logging in user' });
+    }
+};
