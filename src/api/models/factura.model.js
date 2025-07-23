be086e37276289ebf2a61a610d35ec3de1994c37
marceterrone10 <marcelo.terrone10@gmail.com>
2025-07-23 17:06:05 -0300

    facturaSchema and model done

---
+++ src/api/models/factura.model.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+
+const facturaSchema = new mongoose.Schema({
+    fecha: {
+        type: Date,
+        required: true
+    },
+    cliente: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Cliente',
+        required: true
+    },
+    productos: [
+        {
+            productoId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Producto',
+                required: true
+            },
+            nombre: String,
+            cantidad: {
+                type: Number,
+                required: true,
+                min: 1
+            },
+            precioUnitario: {
+                type: Number,
+                required: true,
+                min: 0
+            },
+            subtotal: {
+                type: Number,
+                required: true,
+                min: 0
+            }
+        }
+    ],
+    total: {
+        type: Number,
+        required: true,
+        min: 0
+    }
+});
+
+export default mongoose.model('Factura', facturaSchema);
